68124518e1b6f6b8b62238867f3019b35e1c1708
kittybellth <kittybell.th@gmail.com>
2017-06-19 03:21:58 -0500

    try new way to hook up heroku with sequelize

--- config/connection.js
+++ config/connection.js
@@ -3,16 +3,18 @@ var Sequelize = require("sequelize");
 
 // Creates mySQL connection using Sequelize
 
-var sequelize = new Sequelize('burgers_db', 'root', '', {
-  host: 'localhost',
-  dialect: 'mysql',
+var sequelize = new Sequelize('mysql://ulx3ocn04yxf13iu:x478l6pv5y1oyv9z@y0nkiij6humroewt.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/rywas70n603q816e');
 
-  pool: {
-    max: 5,
-    min: 0,
-    idle: 10000
-  },
-});
+// var sequelize = new Sequelize('burgers_db', 'root', '', {
+//   host: 'localhost',
+//   dialect: 'mysql',
+
+//   pool: {
+//     max: 5,
+//     min: 0,
+//     idle: 10000
+//   },
+// });
 
 // test whether or not it successfully connects
 sequelize
