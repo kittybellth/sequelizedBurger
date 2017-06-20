b8945c41c3bdafb4822c21234fed438497729451
kittybellth <kittybell.th@gmail.com>
2017-06-19 19:56:51 -0500

    changed package.json

--- config/connection.js
+++ config/connection.js
@@ -2,19 +2,16 @@
 var Sequelize = require("sequelize");
 
 // Creates mySQL connection using Sequelize
+var sequelize = new Sequelize('burgers_db', 'root', '', {
+  host: 'localhost',
+  dialect: 'mysql',
 
-var sequelize = new Sequelize('mysql://ulx3ocn04yxf13iu:x478l6pv5y1oyv9z@y0nkiij6humroewt.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/rywas70n603q816e');
-
-// var sequelize = new Sequelize('burgers_db', 'root', '', {
-//   host: 'localhost',
-//   dialect: 'mysql',
-
-//   pool: {
-//     max: 5,
-//     min: 0,
-//     idle: 10000
-//   },
-// });
+  pool: {
+    max: 5,
+    min: 0,
+    idle: 10000
+  },
+});
 
 // test whether or not it successfully connects
 sequelize
